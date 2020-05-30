a5e5d1b2908850a18c4c8679bc8f593a273ed388
Givi Odikadze <givi.odikadze@gmail.com>
2020-05-30 15:57:10 -0700

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,23 @@ import React from 'react'
 import { ExampleComponent } from 'visual-flow'
 import 'visual-flow/dist/index.css'
 
+const data = {
+  nodes: [
+    {
+      id: '1',
+      connections: ['2', '3']
+    },
+    {
+      id: '2'
+    },
+    {
+      id: '3'
+    }
+  ]
+}
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent data={data} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+export interface VisualFlowData {
+  nodes: Node[]
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+interface Node {
+  id: string
+  connections: string[]
+}
+
+interface Props {
+  data: VisualFlowData
+}
+
+// eslint-disable-next-line no-empty-pattern
+export const ExampleComponent = ({ data }: Props) => {
+  return (
+    <div className={styles.visualFlow}>
+      {data.nodes.length}
+      <svg className={styles.designerSvg} width='100%' height='100%'>
+        <g className={styles.designerSvgGroup}> </g>
+      </svg>
+    </div>
+  )
 }
